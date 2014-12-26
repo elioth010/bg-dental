1e6e69112d83012887f9b35f899bbfd803b4504f
Pablo Castellano <pablo@anche.no>
2014-12-26 01:11:14 +0100

    Tabla presupuestos_tratamientos (Pivot)
    Ejemplo:
    $presu2 = Presupuestos::find(9);
    foreach ($presu2->tratamientos as $t) {
            var_dump($t->pivot);
            echo $t->pivot->created_at . '<br>';
            echo $t->pivot->unidades . '<br>';
    }

--- app/models/Presupuestos.php
+++ app/models/Presupuestos.php
@@ -2,9 +2,17 @@
 
 class Presupuestos extends Eloquent {
 
-protected $table = 'presupuestos';
-protected $fillable = array('nombre','aceptado','numerohistoria');
-public function tratamientos(){
-        return $this->belongsToMany('Tratamientos', 'presupuestos_tratamientos', 'tratamiento_id', 'presupuesto_id');
+    protected $table = 'presupuestos';
+    protected $fillable = array('nombre','aceptado','numerohistoria');
+
+    public function tratamientos() {
+        return $this->belongsToMany('Tratamientos', 'presupuestos_tratamientos', 'presupuesto_id', 'tratamiento_id');
+    }
+
+    public function newPivot(Eloquent $parent, array $attributes, $table, $exists) {
+        if ($parent instanceof Tratamientos) {
+            return new PresupuestoTratamiento($parent, $attributes, $table, $exists);
+        }
+        return parent::newPivot($parent, $attributes, $table, $exists);
     }
 }
