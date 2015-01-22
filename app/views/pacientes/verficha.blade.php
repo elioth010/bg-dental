4dd52cdc2b7b75c21d3e3e89ca0eaefcdea8cc7e
Rafa <rafaescribano@bitgeenius.com>
2015-01-22 15:17:16 +0100

    formateo ficha cliente

--- app/views/pacientes/verficha.blade.php
+++ app/views/pacientes/verficha.blade.php
@@ -5,29 +5,25 @@ verficha.blade.php
     @foreach($paciente as $paciente)
 {{ Form::open(array('url'=>'pacientes/$paciente->id/editarficha')) }}     
     <h1>Ficha del paciente:</h1>
-    NHC:
-    {{ Form::text('numerohistoria', $paciente->numerohistoria) }}
+
+<ul>
+
+ 	<li>NHC:{{ Form::text('numerohistoria', $paciente->numerohistoria) }}
     Nombre:
     {{ Form::text('nombre', $paciente->nombre) }}
-    Apellidos:
-    {{ Form::text('apellido1', $paciente->apellido1) }}
-    {{ Form::text('apellido2', $paciente->apellido2) }}
-    <p>NIF:
-    {{ Form::text('NIF', $paciente->NIF) }}
-    <p>Dirección:<p>
-    {{ Form::text('addrnamestre', $paciente->addrnamestre) }}<p>
-    {{ Form::text('direccion', $paciente->direccion) }}<p>
-    {{ Form::text('terrdesc', $paciente->terrdesc) }}<p>
-    {{ Form::text('terrdesc', $paciente->addrpostcode) }}<p>
-    Teléfonos:    
-    
-    {{ Form::text('addrtel2', $paciente->addrtel2) }}<p>
-    {{ Form::text('addrtel2', $paciente->addrtel2) }}<p>
-    Compañía: {{ Form::select('compania', $companias, null) }}
+    Apellidos:    {{ Form::text('apellido1', $paciente->apellido1) }}  {{ Form::text('apellido2', $paciente->apellido2) }}</li>
+    <li>NIF:  {{ Form::text('NIF', $paciente->NIF) }}
+    <li>Dirección:{{ Form::text('addrnamestre', $paciente->addrnamestre) }}</li>
+    <li>{{ Form::text('direccion', $paciente->direccion) }}</li>
+    <li>Ciudad:{{ Form::text('terrdesc', $paciente->terrdesc) }}</li>
+    <li>Código Postal:{{ Form::text('terrdesc', $paciente->addrpostcode) }}</li>
+    <li>Teléfonos: {{ Form::text('addrtel2', $paciente->addrtel2) }}  {{ Form::text('addrtel2', $paciente->addrtel2) }}</li>
+    <li>Compañía: {{ Form::select('compania', $companias, null) }}</li>
     <br>
-    {{--{{ Form::submit('Guardar cambios')}}--}}
-    {{ Form::button('Atrás')}} {{ HTML::link('pacientes/'.$paciente->numerohistoria.'/presupuestos', 'Presupuestos de este paciente') }}
+    <li>{{--{{ Form::submit('Guardar cambios')}}--}}</li>
+    <li>{{ Form::button('Atrás')}} {{ HTML::link('pacientes/'.$paciente->numerohistoria.'/presupuestos', 'Presupuestos de este paciente') }}</li>
 
+</ul>
     @endforeach
 {{ Form::close() }}
 @stop
