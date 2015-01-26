35fb90fb1f0509fa402acf2722864e9354a872eb
Rafa <rafaescribano@bitgeenius.com>
2015-01-26 12:43:17 +0100

    formateo ficha pacientes

--- app/views/pacientes/verficha.blade.php
+++ app/views/pacientes/verficha.blade.php
@@ -6,22 +6,32 @@ verficha.blade.php
 {{ Form::open(array('url'=>'pacientes/$paciente->id/editarficha')) }}     
     <h1>Ficha del paciente:</h1>
 
-<ul>
+<ul class="labelreg4">
+ 	<li>NHC</li>
+    <li>Nombre:</li>
+    <li>Apellidos:</li>
+    <li>NIF:</li>
+    <li>Dirección:</li>
+    <li>Ciudad:</li>
+    <li>Código Postal:</li>
+    <li>Teléfonos:</li>
+    <li>Compañía:</li>
+ 
+</ul>
 
- 	<li>NHC:{{ Form::text('numerohistoria', $paciente->numerohistoria) }}
-    Nombre:
-    {{ Form::text('nombre', $paciente->nombre) }}
-    Apellidos:    {{ Form::text('apellido1', $paciente->apellido1) }}  {{ Form::text('apellido2', $paciente->apellido2) }}</li>
-    <li>NIF:  {{ Form::text('NIF', $paciente->NIF) }}
-    <li>Dirección:{{ Form::text('addrnamestre', $paciente->addrnamestre) }}</li>
-    <li>{{ Form::text('direccion', $paciente->direccion) }}</li>
-    <li>Ciudad:{{ Form::text('terrdesc', $paciente->terrdesc) }}</li>
-    <li>Código Postal:{{ Form::text('terrdesc', $paciente->addrpostcode) }}</li>
-    <li>Teléfonos: {{ Form::text('addrtel2', $paciente->addrtel2) }}  {{ Form::text('addrtel2', $paciente->addrtel2) }}</li>
-    <li>Compañía: {{ Form::select('compania', $companias, null) }}</li>
-    <br>
+<ul class="labelreg3">
+ 	<li>{{ Form::text('numerohistoria', $paciente->numerohistoria) }}</li>
+    <li>{{ Form::text('nombre', $paciente->nombre) }}</li>
+    <li>{{ Form::text('apellido1', $paciente->apellido1) }}  {{ Form::text('apellido2', $paciente->apellido2) }}</li>
+    <li>{{ Form::text('NIF', $paciente->NIF) }}</li>
+    <li>{{ Form::text('addrnamestre', $paciente->addrnamestre) }} {{ Form::text('direccion', $paciente->direccion) }}</li>
+    <li>{{ Form::text('terrdesc', $paciente->terrdesc) }}</li>
+    <li>{{ Form::text('terrdesc', $paciente->addrpostcode) }}</li>
+    <li>{{ Form::text('addrtel2', $paciente->addrtel2) }}  {{ Form::text('addrtel2', $paciente->addrtel2) }}</li>
+    <li>{{ Form::select('compania', $companias, null) }}</li>
+        <br>    
     <li>{{--{{ Form::submit('Guardar cambios')}}--}}</li>
-    <li>{{ Form::button('Atrás')}} {{ HTML::link('pacientes/'.$paciente->numerohistoria.'/presupuestos', 'Presupuestos de este paciente') }}</li>
+    <li>{{ Form::button('Atrás', array('class'=>'botonl'))}} {{ HTML::link('pacientes/'.$paciente->numerohistoria.'/presupuestos', 'Presupuestos de este paciente') }}</li>
 
 </ul>
     @endforeach
