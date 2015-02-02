fbb634fd40db89b502dbe6b760a1733c0d875d4b
Rafa <rafaescribano@bitgeenius.com>
2015-02-02 11:45:37 +0100

    formateo crear pacientes

--- app/views/pacientes/crear.blade.php
+++ app/views/pacientes/crear.blade.php
@@ -9,37 +9,48 @@ crear.blade.php
         @endforeach
 
     @endif
-    <div id="crear">
+
 {{ Form::open(array('url'=>'pacientes/guardar')) }}     
     <h1>Datos del nuevo paciente</h1>
-    NHC:
-    {{ Form::text('numerohistoria') }}
-    Nombre:
-    {{ Form::text('nombre') }}
-    Apellidos:
-    {{ Form::text('apellido1') }}
-    {{ Form::text('apellido2') }}   </p>
-    NIF: {{ Form::text('NIF') }}
-    Fecha de nacimiento: {{ Form::select('dia', range(1, 31) + array(''=>"Día"), Input::old('dia'))}}{{ Form::selectMonth('mes') }}{{ Form::select('ano', range(1900, 2014) + array(''=>"Año"), Input::old('ano'))}}</br>
-    <p>Tipo vía: {{ Form::text('addrnamestre') }}</p>
-    <p>Dirección: {{ Form::text('direccion') }}</p>
-    <p>Provincia: {{ Form::text('terrdesc') }}</p>
-    <p>Código postal: {{ Form::text('addrpostcode') }}</p>
     
-    <p>Teléfono 1: {{ Form::text('addrtel1') }}</p>
-    <p>Teléfono 2: {{ Form::text('addrtel2') }}</p>
-    <p>Correo electrónico: {{ Form::text('mail') }}</p>
 
+<ul class="labelreg5">
+ 	<li>NHC</li>
+    <li>Nombre:</li>
+    <li>Apellidos:</li>
+    <li>NIF:</li>
+    <li>Fecha de nacimiento</li>
+    <li>Tipo de vía</li>
+    <li>Dirección:</li>
+    <li>Ciudad:</li>
+    <li>Código Postal:</li>
+    <li>Teléfonos:</li>
+    <li>Correo electrónico</li>
+    <li>Género</li>
+    <li>Compañía:</li>
+ 
+</ul>
 
-    <p>Mujer: {{ Form::radio('sex', 'mujer') }}</br>
-	Hombre: {{ Form::radio('sex', 'varon') }} </p>
+<ul class="labelreg3">
+ 	<li>{{ Form::text('numerohistoria') }}</li>
+    <li>{{ Form::text('nombre') }}</li>
+    <li>{{ Form::text('apellido1') }} {{ Form::text('apellido2') }}</li>
+    <li>{{ Form::text('NIF') }}</li>
+    <li>{{ Form::select('dia', range(1, 31) + array(''=>"Día"), Input::old('dia'))}}{{ Form::selectMonth('mes') }}{{ Form::select('ano', range(1900, 2014) + array(''=>"Año"), Input::old('ano'))}}</li>
+    <li>{{ Form::text('addrnamestre') }}</li>
+    <li>{{ Form::text('direccion') }}</li>
+    <li>{{ Form::text('terrdesc') }}</li>
+    <li>{{ Form::text('addrpostcode') }}</li>
+    <li>{{ Form::text('addrtel1') }}  {{ Form::text('addrtel2') }}</li>
+    <li>{{ Form::text('mail') }}</li>
+    <li>Mujer: {{ Form::radio('sex', 'mujer') }} Hombre: {{ Form::radio('sex', 'varon') }}</li>
+    <li>{{ Form::select('compania', $companias, null) }} </li>
+        <br>    
+    <li>{{ Form::submit('Guardar cambios', array('class'=>'botonl'))}}</li>
+    <li>{{HTML::link('previous', 'volver')}}</li>
 
-
-    <p>Compañía: {{ Form::select('compania', $companias, null) }}
-    </p></br>
-    {{ Form::submit('Guardar cambios')}}
-    {{HTML::link('previous', 'volver')}}
+</ul>
 
 {{ Form::close() }}
-	</div>
+
 @stop
