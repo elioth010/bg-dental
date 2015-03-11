6b11b51bd4315c9667ad1107a52767feafd5284c
Pablo Castellano <pablo@anche.no>
2015-03-11 13:48:22 +0100

    Limpieza en TratamientosController

--- app/controllers/TratamientosController.php
+++ app/controllers/TratamientosController.php
@@ -30,12 +30,12 @@ class TratamientosController extends \BaseController {
 	public function create()
 	{
 		$grupos = Grupos::lists('nombre', 'id');
-                $tipos_tratamientos = TiposTratamientos::lists('tipo', 'id');
-                $companias = Companias::get();
-		return View::make('tratamientos.crear')
-                        ->with(array('grupos' => $grupos))
-                        ->with(array('tipostratamientos' => $tipos_tratamientos))
-                        ->with(array('companias' => $companias));
+		$tipos_tratamientos = TiposTratamientos::lists('tipo', 'id');
+		$companias = Companias::get();
+
+		return View::make('tratamientos.crear')->with(array('grupos' => $grupos,
+															'tipostratamientos' => $tipos_tratamientos,
+															'companias' => $companias));
 	}
 
 
@@ -46,31 +46,25 @@ class TratamientosController extends \BaseController {
 	 */
 	public function store()
 	{
-            $tratamiento = new Tratamientos;
-            $codigo = Input::get('codigo');
-            $nombre = Input::get('nombre');
-            $grupo = Input::get('grupostratamientos_id');
-            $tipo = Input::get('tipostratamientos_id');
-            $activo = Input::get('activo');
-            $last_id = DB::getPdo()->lastInsertId();
-            $companias = Companias::lists('id');
-            $tratamiento->nombre = $nombre;
-            $tratamiento->codigo = $codigo;
-            $tratamiento->grupostratamientos_id= $grupo;
-            $tratamiento->tipostratamientos_id = $tipo;
-            $tratamiento->activo=$activo;
-            $tratamiento->save();
-            $num_companias = Companias::count();
-            $i = 1;
-            while($i <= $num_companias){
+		$tratamiento = new Tratamientos;
+        $tratamiento->nombre 				= Input::get('nombre');
+        $tratamiento->codigo 				= Input::get('codigo');
+        $tratamiento->grupostratamientos_id = Input::get('grupostratamientos_id');
+        $tratamiento->tipostratamientos_id 	= Input::get('tipostratamientos_id');
+        $tratamiento->activo 				= Input::get('activo');
+        $tratamiento->save();
+
+        $num_companias = Companias::count();
+        $i = 1;
+        while($i <= $num_companias){
             if(Input::has('id-'.$i)){
-            $compania= Input::get('id-'.$i);
-            $precio = Input::get('precio-'.$i);
-            $tratamiento->companias()->attach($compania, array('precio' => $precio));
+	            $compania= Input::get('id-'.$i);
+	            $precio = Input::get('precio-'.$i);
+	            $tratamiento->companias()->attach($compania, array('precio' => $precio));
             }
-            $i++;
-           }
-            //return Redirect::to('tratamientos');
+			$i++;
+		}
+		//return Redirect::to('tratamientos');
 	}
 
 
@@ -86,34 +80,35 @@ class TratamientosController extends \BaseController {
 	}
 
 	public function busqueda()
-	 {
+	{
 		$nombre = Input::get('nombre');
 		if($nombre){
-		$nombre = "%".$nombre."%";
+			$nombre = "%".$nombre."%";
 		}
+
 		$codigo = Input::get('codigo');
 		if($codigo){
-		$codigo = "%".$codigo."%";
+			$codigo = "%".$codigo."%";
 		}
+
 		if($nombre || $codigo) {
-		  $tratamientos = Tratamientos::where('nombre','LIKE', $nombre)->orWhere('codigo','LIKE', $codigo)->get();
-		  foreach($tratamientos as $tratamiento){
-		  $tratamiento_id = $tratamiento->id;
-		  }
-		  $tcp = Precios::leftJoin('tratamientos', 'tratamientos.id', '=', 'tratamientos_id')
-					->leftJoin('companias', 'companias.id','=','companias_id')
-					->select('tratamientos.nombre as nombre_trat', 'companias.nombre as nombre_comp', 'precio', 'tratamientos.id')
-					->where('tratamientos.id' , $tratamiento->id)
-					->get();
+			$tratamientos = Tratamientos::where('nombre','LIKE', $nombre)->orWhere('codigo','LIKE', $codigo)->get();
+			foreach($tratamientos as $tratamiento){
+				$tratamiento_id = $tratamiento->id;
+			}
+			$tcp = Precios::leftJoin('tratamientos', 'tratamientos.id', '=', 'tratamientos_id')
+				->leftJoin('companias', 'companias.id','=','companias_id')
+				->select('tratamientos.nombre as nombre_trat', 'companias.nombre as nombre_comp', 'precio', 'tratamientos.id')
+				->where('tratamientos.id' , $tratamiento->id)
+				->get();
 
 		} else {
-		  return Redirect::to('pacientes/buscar');
+			return Redirect::action('TratamientosController@busqueda');
 		}
-		//var_dump($paciente);
 
+		return View::make('tratamientos.vertratamiento')->with(array('tratamientos' => $tratamientos, 'tcp' => $tcp));
+	}
 
-		return View::make('tratamientos.vertratamiento')->with('tratamientos',$tratamientos)->with('tcp' , $tcp);
-	 }
 	/**
 	 * Show the form for editing the specified resource.
 	 *
@@ -123,41 +118,44 @@ class TratamientosController extends \BaseController {
 	public function edit($id)
 	{
 		$tratamiento = Tratamientos::where('id', $id)->first();
-                $grupos = Grupos::lists('nombre','id');
-                $companias = Companias::get();
+        $grupos = Grupos::lists('nombre','id');
+
 		$precios = Precios::leftJoin('tratamientos', 'tratamientos.id', '=', 'tratamientos_id')
 			->leftJoin('companias', 'companias.id','=','companias_id')
 			->select('companias.nombre as nombre_comp', 'companias.id', 'precio', 'grupostratamientos_id', 'tipostratamientos_id')
 			->where('tratamientos.id' , $tratamiento->id)
 			->get();
-                $companias = Companias::lists('nombre', 'id');
-                var_dump($companias);
-                $tipos = DB::table('tipostratamientos')->get();
-		return View::make('tratamientos.editar')->with('tratamiento', $tratamiento)->with('tcp' , $precios)->with('grupos', $grupos)->with('tipos', $tipos)->with('companias', $companias);
+
+        $companias = Companias::lists('nombre', 'id');
+        $tipos = DB::table('tipostratamientos')->get();
+
+		return View::make('tratamientos.editar')->with(array('tratamiento' => $tratamiento, 'tcp' => $precios,
+															 'grupos' => $grupos, 'tipos' => $tipos,
+															 'companias' => $companias));
 	}
 
 	public function guardar_t($id){
-                $codigo = Input::get('codigo');
-                $grupostratamientos_id = Input::get('grupostratamientos_id');
-                $tipostratamientos_id = Input::get('tipotratamiento');
-                $tratamiento = Tratamientos::find($id);
-                $tratamiento->grupostratamientos_id = $grupostratamientos_id;
-                $tratamiento->tipostratamientos_id = $tipostratamientos_id;
-                $tratamiento->save();
-                $num_companias = Companias::count();
-                $i = 1;
-                while($i <= $num_companias){
-                if(Input::has('precio-'.$i)){
-                $compania= Input::get('cid-'.$i);
-                $precio = Input::get('precio-'.$i);
-                $tratamiento->companias($compania)->detach($compania);
-                $tratamiento->companias()->attach($compania, array('precio' => $precio));
-                }
-                $i++;
-               }
+		//$codigo = Input::get('codigo');
 
-                //$tratamientoaguardado = Tratamientos::where('id',$id)->first();
-                //return Redirect::to('tratamientos');
+		$tratamiento = Tratamientos::find($id);
+		$tratamiento->grupostratamientos_id = Input::get('grupostratamientos_id');
+		$tratamiento->tipostratamientos_id 	= Input::get('tipotratamiento');
+		$tratamiento->save();
+
+		$num_companias = Companias::count();
+		$i = 1;
+		while($i <= $num_companias){
+			if(Input::has('precio-'.$i)){
+				$compania= Input::get('cid-'.$i);
+				$precio = Input::get('precio-'.$i);
+				$tratamiento->companias($compania)->detach($compania);
+				$tratamiento->companias()->attach($compania, array('precio' => $precio));
+			}
+			$i++;
+		}
+
+		//$tratamientoaguardado = Tratamientos::where('id',$id)->first();
+		//return Redirect::to('tratamientos');
 
 	}
 
