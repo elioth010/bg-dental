2b0830a723abe589e2d9b27f65c252cf590cd3d4
Pablo Castellano <pablo@anche.no>
2015-03-11 14:32:18 +0100

    Tratamientos: Tabla que mantiene el header fijo

--- app/views/tratamientos/index.blade.php
+++ app/views/tratamientos/index.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.main')
 
+@section('ceeseeses')
+<style>
+	tbody {
+		height: 300px;
+		overflow: auto;
+	}
+
+	td {
+		padding: 3px 10px;
+	}
+
+	thead > tr, tbody {
+		display:block;
+	}
+</style>
+@stop
+
 @section('contenido')
 <<<<<<< HEAD
 	<div class="search">
@@ -56,28 +73,35 @@ $(window).resize(function() {
 <div class="search">
 	{{ HTML::linkAction('TratamientosController@show', 'Buscar tratamientos') }}</div>
 	<h1>Tratamientos</h1>
-	<table>
-		<tr>
-			<th>Código</th>
-			<th>Nombre tratamiento</th>
-			@foreach($companias as $compania)
-			<th>{{$compania}}</th>
-			@endforeach
-		</tr>
-	</table>
-	<table>
-		@foreach($tratamientos as $tratamiento)
-		<tr title="{{$tratamiento->nombre}}">
 
-			<td>{{$tratamiento->codigo}}</td>
-			<td>{{ HTML::linkAction('TratamientosController@edit', $tratamiento->nombre, $tratamiento->id) }}</td>
+	<table>
+	    <thead>
+	        <tr>
+	            <td>Código</td>
+	            <td>Nombre tratamiento</td>
+				@foreach($companias as $compania)
+				<td>{{ $compania }}</td>
+				@endforeach
+	        </tr>
+	    </thead>
+	    <tbody>
+			@foreach($tratamientos as $tratamiento)
+			<?php $precios = explode(",", $tratamiento->precios);
+			// HACK para no mostrar los tratamientos sin precios
+			if (!empty($precios[0])) {
+			?>
+			<tr title="{{ $tratamiento->nombre }}">
 
-			<?php $precios = explode(",", $tratamiento->precios);?>
-			@foreach($precios as $precio)
-			<td>{{$precio.'€'}}</td>
+				<td>{{ $tratamiento->codigo }}</td>
+				<td>{{ HTML::linkAction('TratamientosController@edit', $tratamiento->nombre, $tratamiento->id) }}</td>
+
+				@foreach($precios as $precio)
+				<td>{{ $precio.'€' }}</td>
+				@endforeach
+			</tr>
+			<?php } ?>
 			@endforeach
-		</tr>
-		@endforeach
+	    </tbody>
 	</table>
 >>>>>>> 9e9b5c1a4017df9153430ec4497ab913f50f089d
 @stop
