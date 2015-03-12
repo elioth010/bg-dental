d6b18c290590ac73280b33396cc917803f04b655
ban007 <ban0072000@gmail.com>
2015-03-12 18:38:32 +0100

    mapeado nuevo de la imagen

--- app/views/presupuestos/odontograma.blade.php
+++ app/views/presupuestos/odontograma.blade.php
@@ -1,37 +1,37 @@
 @section('odontograma')
 	{{ HTML::image('/imagenes/odontograma_750.jpg', 'Odontograma', array('usemap' => '#odontograma', 'class' => 'odontograma', 'id' => 'iodontograma')) }}
 	<map id="odontograma" name="odontograma">
-		<area id="p18" shape="rect" alt="pieza 18" title="Pieza 18" coords="2,27,54,285" href="#" />
+		<area id="p18" shape="rect" alt="pieza 18" title="Pieza 18" coords="2,27,59,287" href="#" />
 		<area id="p17" shape="rect" alt="pieza 17" title="Pieza 17" coords="59,27,106,287" href="#" />
-		<area id="p16" shape="rect" alt="pieza 16" title="Pieza 16" coords="109,27,164,285" href="#" />
+		<area id="p16" shape="rect" alt="pieza 16" title="Pieza 16" coords="106,27,164,287" href="#" />
 		<area id="p15" shape="rect" alt="pieza 15" title="Pieza 15" coords="168,27,207,287" href="#" />
 		<area id="p14" shape="rect" alt="pieza 14" title="Pieza 14" coords="212,27,244,287" href="#" />
-		<area id="p13" shape="rect" alt="pieza 13" title="Pieza 13" coords="250,27,291,285" href="#" />
-		<area id="p12" shape="rect" alt="pieza 12" title="Pieza 12" coords="293,27,332,286" href="#" />
-		<area id="p11" shape="rect" alt="pieza 11" title="Pieza 11" coords="336,27,372,285" href="#" />
-		<area id="p21" shape="rect" alt="pieza 21" title="Pieza 21" coords="377,27,416,286" href="#" />
-		<area id="p22" shape="rect" alt="pieza 22" title="Pieza 22" coords="418,27,454,286" href="#" />
-		<area id="p23" shape="rect" alt="pieza 23" title="Pieza 23" coords="458,29,502,285" href="#" />
-		<area id="p24" shape="rect" alt="pieza 24" title="Pieza 24" coords="504,29,541,285" href="#" />
-		<area id="p25" shape="rect" alt="pieza 25" title="Pieza 25" coords="543,30,582,286" href="#" />
-		<area id="p26" shape="rect" alt="pieza 26" title="Pieza 26" coords="585,27,641,286" href="#" />
-		<area id="p27" shape="rect" alt="pieza 27" title="Pieza 27" coords="642,28,692,284" href="#" />
-		<area id="p28" shape="rect" alt="pieza 28" title="Pieza 28" coords="694,28,744,282" href="#" />
+		<area id="p13" shape="rect" alt="pieza 13" title="Pieza 13" coords="250,27,291,287" href="#" />
+		<area id="p12" shape="rect" alt="pieza 12" title="Pieza 12" coords="293,27,332,287" href="#" />
+		<area id="p11" shape="rect" alt="pieza 11" title="Pieza 11" coords="336,27,372,287" href="#" />
+		<area id="p21" shape="rect" alt="pieza 21" title="Pieza 21" coords="377,27,416,287" href="#" />
+		<area id="p22" shape="rect" alt="pieza 22" title="Pieza 22" coords="418,27,454,287" href="#" />
+		<area id="p23" shape="rect" alt="pieza 23" title="Pieza 23" coords="458,27,502,287" href="#" />
+		<area id="p24" shape="rect" alt="pieza 24" title="Pieza 24" coords="504,27,541,287" href="#" />
+		<area id="p25" shape="rect" alt="pieza 25" title="Pieza 25" coords="543,27,582,287" href="#" />
+		<area id="p26" shape="rect" alt="pieza 26" title="Pieza 26" coords="585,27,641,287" href="#" />
+		<area id="p27" shape="rect" alt="pieza 27" title="Pieza 27" coords="642,27,692,287" href="#" />
+		<area id="p28" shape="rect" alt="pieza 28" title="Pieza 28" coords="694,27,744,287" href="#" />
 		<area id="p48" shape="rect" alt="pieza 48" title="Pieza 48" coords="2,295,55,546" href="#" />
 		<area id="p47" shape="rect" alt="pieza 47" title="Pieza 47" coords="59,295,109,547" href="#" />
-		<area id="p46" shape="rect" alt="pieza 46" title="Pieza 46" coords="110,294,170,548" href="#" />
-		<area id="p45" shape="rect" alt="pieza 45" title="Pieza 45" coords="173,295,208,548" href="#" />
-		<area id="p44" shape="rect" alt="pieza 44" title="Pieza 44" coords="211,294,248,550" href="#" />
+		<area id="p46" shape="rect" alt="pieza 46" title="Pieza 46" coords="110,295,170,550" href="#" />
+		<area id="p45" shape="rect" alt="pieza 45" title="Pieza 45" coords="173,295,208,550" href="#" />
+		<area id="p44" shape="rect" alt="pieza 44" title="Pieza 44" coords="211,295,248,550" href="#" />
 		<area id="p43" shape="rect" alt="pieza 43" title="Pieza 43" coords="252,295,290,550" href="#" />
-		<area id="p42" shape="rect" alt="pieza 42" title="Pieza 42" coords="293,293,332,548" href="#" />
-		<area id="p41" shape="rect" alt="pieza 41" title="Pieza 41" coords="336,295,370,549" href="#" />
+		<area id="p42" shape="rect" alt="pieza 42" title="Pieza 42" coords="293,295,332,550" href="#" />
+		<area id="p41" shape="rect" alt="pieza 41" title="Pieza 41" coords="336,295,370,550" href="#" />
 		<area id="p31" shape="rect" alt="pieza 31" title="Pieza 31" coords="376,295,414,550" href="#" />
-		<area id="p32" shape="rect" alt="pieza 32" title="Pieza 32" coords="414,295,454,549" href="#" />
+		<area id="p32" shape="rect" alt="pieza 32" title="Pieza 32" coords="414,295,454,550" href="#" />
 		<area id="p33" shape="rect" alt="pieza 33" title="Pieza 33" coords="456,295,499,550" href="#" />
-		<area id="p34" shape="rect" alt="pieza 34" title="Pieza 34" coords="502,296,539,549" href="#" />
-		<area id="p35" shape="rect" alt="pieza 35" title="Pieza 35" coords="542,296,574,550" href="#" />
-		<area id="p36" shape="rect" alt="pieza 36" title="Pieza 36" coords="576,295,634,548" href="#" />
-		<area id="p37" shape="rect" alt="pieza 37" title="Pieza 37" coords="636,295,690,551" href="#" />
-		<area id="p38" shape="rect" alt="pieza 38" title="Pieza 38" coords="693,295,743,547" href="#" />
+		<area id="p34" shape="rect" alt="pieza 34" title="Pieza 34" coords="502,295,539,550" href="#" />
+		<area id="p35" shape="rect" alt="pieza 35" title="Pieza 35" coords="542,295,574,550" href="#" />
+		<area id="p36" shape="rect" alt="pieza 36" title="Pieza 36" coords="576,295,634,550" href="#" />
+		<area id="p37" shape="rect" alt="pieza 37" title="Pieza 37" coords="636,295,690,550" href="#" />
+		<area id="p38" shape="rect" alt="pieza 38" title="Pieza 38" coords="693,295,743,550" href="#" />
 	</map>
 @stop
