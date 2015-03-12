ae16266e10935a71c8e3f9095783c63765b58c25
Pablo Castellano <pablo@anche.no>
2015-03-12 12:15:31 +0100

    Solucionado el bug de Precios::. Problema de namespace
    Ejecutar:
    php composer.phar  dump-autoload
    php artisan  migrate:reset
    php artisan  migrate

--- app/database/migrations/2015_03_11_103123_precios_preciofinal.php
+++ app/database/migrations/2015_03_11_103123_precios_preciofinal.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Precios extends Migration {
+class PreciosPreciofinal extends Migration {
 
 	/**
 	 * Run the migrations.
