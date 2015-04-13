8318693d6948a0b787c61626a3d05a6deb1e4b25
ban007 <ban0072000@gmail.com>
2015-04-13 12:24:11 +0200

    logo quirón

--- app/views/profesionales/index.blade.php
+++ app/views/profesionales/index.blade.php
@@ -7,8 +7,7 @@
   
     <table border = "1">
       <tr>
-      <th>Id
-      </th><th>Nombre
+      <th>Nombre
       </th><th>Apellidos
       </th><th>Especialidad
       </th><th>Sede(s)
@@ -17,7 +16,7 @@
       
       @foreach($profesionales as $profesional)
         <tr>
-        <td>{{$profesional->id}}</td>
+        
         <td>{{$profesional->nombre}}</td>
         <td>{{$profesional->apellido1}}, {{$profesional->apellido2}}</td>
         <td>{{$profesional->especialidad}}</td>
