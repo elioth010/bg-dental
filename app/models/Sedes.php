6a6d9e6de191bcbd0d77d9ff6cbc123b02428dbb
ban007 <ban0072000@gmail.com>
2015-04-20 12:30:52 +0200

    añadido campo de cuenta bancaria en sedes y todos sus formularios

--- app/models/Sedes.php
+++ app/models/Sedes.php
@@ -1,7 +1,7 @@
 <?php
 class Sedes extends Eloquent {
 protected $table = 'sedes';
-protected $fillable = array('nombre', 'calleynum', 'cp', 'ciudad', 'provincia', 'mail', 'tel');
+protected $fillable = array('nombre', 'calleynum', 'cp', 'ciudad', 'provincia', 'mail', 'tel', 'cuenta');
 
 public function users() {
         return $this->belongsToMany('Users', 'sedes_users', 'sede_id', 'user_id');
