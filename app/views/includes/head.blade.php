b2b2d87b4147efff2e0a5d55a2f1fb1f1166dcde
ban007 <ban0072000@gmail.com>
2015-05-14 12:26:31 +0200

    eliminado del footer un javascript

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -2,8 +2,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/js/jquery-1.11.0.min.js"></script>
-	<script src="/js/jquery-migrate-1.2.1.min.js"></script>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+    <script src="/js/jquery-migrate-1.2.1.min.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
     <link rel="icon" type="image/png" sizes="16x16" href="/imagenes/favicon.png" /> 
-	<link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
     {{ HTML::style('css/main.css') }}
