dfc79e980539b2219a8885d82389aa17b94ad38d
ban007 <ban0072000@gmail.com>
2015-05-18 21:17:45 +0200

    estadísticas y facturación

---
+++ app/views/estadisticas/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.main')
+ 
+@section('contenido')
+ <h3>Estadísticas:</h3>
+ Elegir intervalo de tiempo:
+ {{ Form::open(array('url'=>'estadisticas')) }}
+ {{ Form::text('fecha_inicio', '', array('id' => 'datepicker', 'class' => 'euros')) }} - {{ Form::text('fecha_fin', '', array('id' => 'datepicker', 'class' => 'euros')) }}
+ {{ Form::submit('OK', array('class'=>'botonl'))}}
+ {{ Form::close() }}
+ 
+ 
+ </br>
+ @foreach($profesionales as $profesional)
+ <h3>Estadísticas de facturación mes actual:</h3></br>
+ {{$profesional->apellido1}}
+ <table>
+     <tr>
+         <th>Fecha realización</th>
+         <th>Tratamiento</th>
+         <th>precio</th>
+     </tr>
+ @foreach($facturacion as $item)
+ <tr>
+     <td>{{$item->fecha_realizacion}}</td>
+     <td>{{$item->t_n}}</td>
+     <td>{{$item->precio}}
+    @endforeach
+ </table>   
+ @endforeach
+
+   
+@stop
--- app/views/facturacion/index.blade.php
+++ app/views/facturacion/index.blade.php
@@ -15,31 +15,36 @@
             <th>Cobrado por profesional</th>
             <th>Guardar</th>
         </tr>
+        <?php $i = 1;?>
+        
         @foreach($historiales as $historial)
-        <tr>
-            <td>{{ HTML::link('historial_clinico/'.$historial->p_id, $historial->p_n.', '.$historial->p_a1.' '.$historial->p_a2)}} </td>
-            <td>{{$historial->t_n }}</td>
-            <td class = "td_centrado">{{$historial->fecha_realizacion}}</td>
-            {{ Form::open(array('url'=>'facturacion/'.$historial->id, 'method' => 'put')) }}
-            <td class = "td_centrado">
-                @if($historial->abonado_quiron != 1)
-                {{Form::checkbox('abonado_quiron')  }}
-                @else
-                {{Form::checkbox('abonado_quiron',1, true)  }}
-                @endif
-            </td>
-            
-            <td class = "td_centrado">
-                @if($historial->cobrado_profesional != 1)
-                {{Form::checkbox('cobrado_profesional')  }}
-                @else
-                {{Form::checkbox('cobrado_profesional',1, true)  }}
-                @endif
-            </td>
-            <td> {{ Form::submit('OK', array('class'=>'botonl'))}}</td>
-            {{Form::close()}}
-        </tr>
+            <tr>
+                <td>{{ HTML::link('historial_clinico/'.$historial->p_id, $historial->p_n.', '.$historial->p_a1.' '.$historial->p_a2)}} </td>
+                <td>{{$historial->t_n }}</td>
+                <td class = "td_centrado">{{$historial->fecha_realizacion}}</td>
+                {{ Form::open(array('url'=>'facturacion/'.$historial->h_id, 'method' => 'put')) }}
+                {{Form::hidden('id', $historial->h_id)}}
+                    <td class = "td_centrado">
+                        @if($historial->abonado_quiron != 1)
+                        {{Form::checkbox('abonado_quiron-'.$i)  }}
+                        @else
+                        {{Form::checkbox('abonado_quiron-'.$i,1, true)  }}
+                        @endif
+                    </td>
+
+                    <td class = "td_centrado">
+                        @if($historial->cobrado_profesional != 1)
+                        {{Form::checkbox('cobrado_profesional-'.$i)  }}
+                        @else
+                        {{Form::checkbox('cobrado_profesional-'.$i,1, true)  }}
+                        <?php $i++;?>
+                        @endif
+                    </td>
+                    <td> 
+            </tr>
         @endforeach
+        {{ Form::submit('OK', array('class'=>'botonl'))}}</td>
+                {{Form::close()}}
     </table>
 	</div>
 </div>
