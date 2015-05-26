7e06c47609e2c7b41469f365759432c22493f9ea
Pablo Castellano <pablo@anche.no>
2015-05-26 14:18:28 +0200

    añado campo precio al historial_clinico

---
+++ app/database/migrations/2015_05_26_121647_add_precio_historial_clinico.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPrecioHistorialClinico extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('historial_clinico', function(Blueprint $table)
+		{
+			$table->float('precio');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('historial_clinico', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
