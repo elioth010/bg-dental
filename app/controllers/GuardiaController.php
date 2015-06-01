2bad97c7521b3edac05c8065ae150d7544678f42
Pablo Castellano <pablo@anche.no>
2015-06-01 12:37:24 +0200

    Indent y Refactor: getGuardiaCalendar

--- app/controllers/GuardiaController.php
+++ app/controllers/GuardiaController.php
@@ -6,35 +6,29 @@ class GuardiaController extends \BaseController {
     public function index()
     {
         $sede_id = Auth::user()->sede_id;
-        if($sede_id != 4){
-        if(null !== Input::get('cdate')){
-        $cdate = explode("-", Input::get('cdate'));
-        $mes = $cdate[1];
-        $ano = $cdate[0];
-        } else {
-            $mes = date("m");
-            $ano = date("Y");
-        }
-        $eventos = Guardias::where('fecha_guardia', 'LIKE', '%-'.$mes.'-%')->get(array('fecha_guardia', 'profesional_id'));
-        $events = array();
-        foreach($eventos as $evento){
-            $profesionales = Profesional::find($evento->profesional_id);
-            $events[$evento->fecha_guardia] = $profesionales->nombre.", ".$profesionales->apellido1;
-        }
-        //var_dump($events);
-         $cal = Calendar::make();
-         //$cal->setEvents($events);
-         $cal->setDayLabels(array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'));
-         //$cal->setStartWeek('L');
-         $cal->setBasePath('/guardia'); // Base path for navigation URLs
-         $cal->setDate(Input::get('cdate')); //Set starting date
-         $cal->showNav(true); // Show or hide navigation
-         $cal->setMonthLabels(array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')); //Month names
-         $cal->setEvents($events); // Receives the events array
-         $cal->setTableClass('table_cal'); //Set the table's class name
-         $calendario = $cal->generate();
 
-         return View::make('agenda.guardias')->with('calendario' , $calendario);
+        if ($sede_id != 4) {
+            if(null !== Input::get('cdate')){
+                $cdate = explode("-", Input::get('cdate'));
+                $mes = $cdate[1];
+                $ano = $cdate[0];
+            } else {
+                $mes = date("m");
+                $ano = date("Y");
+            }
+
+            $eventos = Guardias::where('fecha_guardia', 'LIKE', '%-'.$mes.'-%')->get(array('fecha_guardia', 'profesional_id'));
+            $events = array();
+            foreach($eventos as $evento){
+                $profesionales = Profesional::find($evento->profesional_id);
+                $events[$evento->fecha_guardia] = $profesionales->nombre.", ".$profesionales->apellido1;
+            }
+
+            $calendario = $this->getGuardiaCalendar($events);
+
+            // dummy:
+            $sede = Sedes::where('id', 1)->firstOrFail();
+            return View::make('agenda.guardias')->with(array('calendario' => $calendario, 'sede' => $sede));
         } else {
             $sedes = Sedes::lists('nombre', 'id');
 
@@ -61,18 +55,8 @@ class GuardiaController extends \BaseController {
             $profesionales = Profesional::find($evento->profesional_id);
             $events[$evento->fecha_guardia] = $profesionales->nombre.", ".$profesionales->apellido1;
         }
-        //var_dump($events);
-        $cal = Calendar::make();
-        //$cal->setEvents($events);
-        $cal->setDayLabels(array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'));
-        //$cal->setStartWeek('L');
-        $cal->setBasePath('/guardia'); // Base path for navigation URLs
-        $cal->setDate(Input::get('cdate')); //Set starting date
-        $cal->showNav(true); // Show or hide navigation
-        $cal->setMonthLabels(array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')); //Month names
-        $cal->setEvents($events); // Receives the events array
-        $cal->setTableClass('table_cal'); //Set the table's class name
-        $calendario = $cal->generate();
+
+        $calendario = $this->getGuardiaCalendar($events);
 
         return View::make('agenda.guardias')->with('calendario' , $calendario)->with('sede', $sede);
     }
@@ -118,17 +102,10 @@ class GuardiaController extends \BaseController {
         //                  $input = "<input  type = \"hidden\" name=\"dia\" value= ".$date_in.">";
         //                  $events[$date_in] = array($input, $select_prof);
         //            }
-         $cal = Calendar::make();
-         $cal->setDayLabels(array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'));
-         //$cal->setStartWeek('L');
-         $cal->setBasePath('/guardia/create'); // Base path for navigation URLs
-         $cal->setDate(Input::get('cdate')); //Set starting date
-         $cal->showNav(true); // Show or hide navigation
-         $cal->setMonthLabels(array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')); //Month names
-         $cal->setEvents($events); // Receives the events array
-         $cal->setTableClass('table_cal'); //Set the table's class name
-         $calendario = $cal->generate();
-         return View::make('agenda.crear_guardias')->with('calendario', $calendario)->with('numero_dias', $numero)->with('sede', $sede);
+
+        $calendario = $this->getGuardiaCalendar($events, '/guardia/create');
+
+        return View::make('agenda.crear_guardias')->with('calendario', $calendario)->with('numero_dias', $numero)->with('sede', $sede);
         }
     }
 
@@ -170,16 +147,8 @@ class GuardiaController extends \BaseController {
         //                  $input = "<input  type = \"hidden\" name=\"dia\" value= ".$date_in.">";
         //                  $events[$date_in] = array($input, $select_prof);
         //            }
-        $cal = Calendar::make();
-        $cal->setDayLabels(array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'));
-        //$cal->setStartWeek('L');
-        $cal->setBasePath('/guardia/create'); // Base path for navigation URLs
-        $cal->setDate(Input::get('cdate')); //Set starting date
-        $cal->showNav(true); // Show or hide navigation
-        $cal->setMonthLabels(array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')); //Month names
-        $cal->setEvents($events); // Receives the events array
-        $cal->setTableClass('table_cal'); //Set the table's class name
-        $calendario = $cal->generate();
+
+        $calendario = $this->getGuardiaCalendar($events, '/guardia/create');
 
         return View::make('agenda.crear_guardias')->with('calendario', $calendario)->with('numero_dias', $numero)->with('sede', $sede);
     }
@@ -210,6 +179,20 @@ class GuardiaController extends \BaseController {
         //
     }
 
+
+    private function getGuardiaCalendar($events, $basepath = '/guardia') {
+        $cal = Calendar::make();
+        $cal->setDayLabels(array('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'));
+        //$cal->setStartWeek('L');
+        $cal->setBasePath($basepath); // Base path for navigation URLs
+        $cal->setDate(Input::get('cdate')); //Set starting date
+        $cal->showNav(true); // Show or hide navigation
+        $cal->setMonthLabels(array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')); //Month names
+        $cal->setEvents($events); // Receives the events array
+        $cal->setTableClass('table_cal'); //Set the table's class name
+
+        return $cal->generate();
+    }
 
     public function update($id)
     {
