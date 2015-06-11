de765868cb11fc6f55f5039b6b3f8d38e154d23c
Pablo Castellano <pablo@anche.no>
2015-06-11 17:32:22 +0200

    "nombre del profesional"

--- app/views/presupuestos/imprimirpresupuesto.blade.php
+++ app/views/presupuestos/imprimirpresupuesto.blade.php
@@ -155,7 +155,7 @@ q:before, q:after {
         <li>Dirección: {{ $paciente->Direccion }}
         {{ $paciente->addrnamestre }} {{ $paciente->addrpostcode }} </li>
         <li>Teléfono: {{ $paciente->addrtel1 }} {{ $paciente->addrtel2 }}</li>
-        <li>Nombre del Profesional: Dra./Dr. {{$presupuesto->p_a1}} {{$presupuesto->p_a2}}</li>
+        <li>Dra./Dr. {{$presupuesto->p_a1}} {{$presupuesto->p_a2}}</li>
 	<li class="vista">
     @if ($showpdf)
     PDF: {{ HTML::linkAction('PresupuestosController@imprimirPDF', 'Descargar', array($paciente->numerohistoria, $presupuesto->id), ['target'=>'_blank']) }}
