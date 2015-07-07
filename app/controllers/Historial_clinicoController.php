4687951883d49e0cbf6e5d327df151407541b52a
ban007 <ban0072000@gmail.com>
2015-07-07 10:50:33 +0200

    eliminación de líneas en HC

--- app/controllers/Historial_clinicoController.php
+++ app/controllers/Historial_clinicoController.php
@@ -349,17 +349,19 @@ class Historial_clinicoController extends \BaseController {
     public function destroy()
     {
         //buscamos el historial para antes de borrarlo marcar lo tratamientos como no realizados si es que vienen de un presu para que puedan volver a ser adjuntados
-        //$historial = Historial_clinico::where('historial_clinico.id', Input::get('h_id'))->leftJoin('presupuestos_tratamientos', 'historial_clinico.presupuesto_tratamiento_id', '=', 'presupuestos_tratamientos.id')
-//                                        ->firstorFail();
+        $historial = Historial_clinico::where('historial_clinico.id', Input::get('h_id'))->firstorFail();
+        
         //ahora deberíamos encontrar en la tabla presupuestos_tratamientos el $historial->presupuesto_tratamiento_id para marcarlo como 0 en "presupuestos_tratamientos.estado".
         //aquí hay un error que tiene que ver con el model. El model presupuesotTratamiento extends Pivot y no sé como hacerlo reversible, es decir hacer lo siguiente, que me da error:
                                 
-        //$presu_trat = PresupuestoTratamiento::where('presupuestos_tratamientos.id', $historial->presupuesto_tratamiento_id)->get();
-//          $presu_trat->estado = 0;
-//          $presu_trat->update();
-        
+        $presu_trat = DB::table('presupuestos_tratamientos')->where('id', $historial->presupuesto_tratamiento_id)->update(array('estado' => 0));
+//        $presu_trat->estado = 0;
+//        $presu_trat->update();
+        $Paciente_id = $historial->paciente_id;
+        $historial->delete();
+        return Redirect::action('Historial_clinicoController@show', $Paciente_id);
                         
-        return Redirect::action('Historial_clinicoController@show', Input::get('h_p_id'))->with('message', 'Función no disponible - En pruebas');
+        //return Redirect::action('Historial_clinicoController@show', Input::get('h_p_id'))->with('message', 'Función no disponible - En pruebas');
         
     }
 
