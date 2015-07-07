50fa6e72bb0c4e1bcc0848e54771d1a4181b2c92
Pablo Castellano <pablo@anche.no>
2015-07-07 20:55:00 +0200

    Estadisticas, cobros y facturacion: formularios GET y unifico los metodos con fecha (_cf) y sin fecha

--- app/controllers/CobrosController.php
+++ app/controllers/CobrosController.php
@@ -9,55 +9,48 @@ class CobrosController extends \BaseController {
 	 */
 	public function index()
 	{
-        $cobros = Cobros::leftJoin('tipos_de_cobro', 'tipos_de_cobro.id', '=', 'cobros.tipos_de_cobro_id')
-                    ->leftJoin('pacientes', 'pacientes.id', '=', 'cobros.paciente_id')
-                    ->select('cobros.*', 'tipos_de_cobro.nombre as tc_n', 'pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', DB::raw("DATE_FORMAT(cobros.created_at, '%d/%m/%Y') as creado"))
-                    ->get();
-        return View::make('cobros.index')->with('cobros', $cobros);
-
-	}
-
-    public function index_cf()
-	{
-        if (!empty($_POST)){
+		if (Input::has('fecha_inicio') && Input::get('fecha_fin')){
             $fecha_inicio = Input::get('fecha_inicio');
-            $fecha_inicio = explode('/', $fecha_inicio);
-            $fecha_inicio = $fecha_inicio[2]."-".$fecha_inicio[1]."-".$fecha_inicio[0];
             $fecha_fin = Input::get('fecha_fin');
-            $fecha_fin = explode('/', $fecha_fin);
-            $fecha_fin = $fecha_fin[2]."-".$fecha_fin[1]."-".$fecha_fin[0];
-//                var_dump($fecha_fin);
-//                var_dump($fecha_inicio);
-            //echo "POST";
         } else {
-            $fecha_fin = date('Y-m-d');
-            $fecha_primeros = date('Y-m-');
-            $fecha_inicio = $fecha_primeros."1";
-//                var_dump($fecha_fin);
-//                var_dump($fecha_inicio);
-//                echo "Sin POST";
+            $fecha_fin = date('d/m/Y');
+            $fecha_primeros = date('/m/Y');
+            $fecha_inicio = "1".$fecha_primeros;
         }
-        var_dump($fecha_inicio);
-                    var_dump($fecha_fin);
-        $cobros = Cobros::where('cobros.created_at', '>=', $fecha_inicio)
-                    ->where(DB::raw('CAST(cobros.created_at as DATE)'),  '<=', $fecha_fin)
+
+		$fecha_inicio_iso = explode('/', $fecha_inicio);
+		$fecha_inicio_iso = $fecha_inicio_iso[2]."-".$fecha_inicio_iso[1]."-".$fecha_inicio_iso[0];
+		$fecha_fin_iso = explode('/', $fecha_fin);
+		$fecha_fin_iso = $fecha_fin_iso[2]."-".$fecha_fin_iso[1]."-".$fecha_fin_iso[0];
+
+        $cobros = Cobros::where('cobros.created_at', '>=', $fecha_inicio_iso)
+                    ->where(DB::raw('CAST(cobros.created_at as DATE)'),  '<=', $fecha_fin_iso)
                     ->leftJoin('tipos_de_cobro', 'tipos_de_cobro.id', '=', 'cobros.tipos_de_cobro_id')
                     ->leftJoin('pacientes', 'pacientes.id', '=', 'cobros.paciente_id')
                     ->select('cobros.*', 'tipos_de_cobro.nombre as tc_n', 'pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', DB::raw("DATE_FORMAT(cobros.created_at, '%d/%m/%Y') as creado"))
                     ->get();
+        return View::make('cobros.index')->with('cobros', $cobros)->with('fecha_inicio', $fecha_inicio)->with('fecha_fin', $fecha_fin);
 
-
-
-//            $cobros = Cobros::whereBetween('cobros.created_at', array($fecha_inicio, $fecha_fin))->leftJoin('tipos_de_cobro', 'tipos_de_cobro.id', '=', 'cobros.tipos_de_cobro_id')
-//                        ->leftJoin('pacientes', 'pacientes.id', '=', 'cobros.paciente_id')
-//                        ->select('cobros.*', 'tipos_de_cobro.nombre as tc_n', 'pacientes.nombre as p_n', DB::raw("DATE_FORMAT(cobros.created_at, '%d/%m/%Y') as creado"))
-//                        ->get();
-        return View::make('cobros.index')->with('cobros', $cobros);
 	}
+
 
     public function morosos()
 	{
-        $p_d_c = Historial_clinico::where('pendiente_de_cobro', 1)
+		if (Input::has('fecha_inicio') && Input::get('fecha_fin')){
+            $fecha_inicio = Input::get('fecha_inicio');
+            $fecha_fin = Input::get('fecha_fin');
+        } else {
+            $fecha_fin = date('d/m/Y');
+            $fecha_primeros = date('/m/Y');
+            $fecha_inicio = "1".$fecha_primeros;
+        }
+
+		$fecha_inicio_iso = explode('/', $fecha_inicio);
+		$fecha_inicio_iso = $fecha_inicio_iso[2]."-".$fecha_inicio_iso[1]."-".$fecha_inicio_iso[0];
+		$fecha_fin_iso = explode('/', $fecha_fin);
+		$fecha_fin_iso = $fecha_fin_iso[2]."-".$fecha_fin_iso[1]."-".$fecha_fin_iso[0];
+
+        $p_d_c = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))->where('pendiente_de_cobro', 1)
                 ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
                 ->select('historial_clinico.*', 'pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2',
                         DB::raw("DATE_FORMAT(historial_clinico.fecha_realizacion, '%d/%m/%Y') as fecha"))
@@ -67,22 +60,7 @@ class CobrosController extends \BaseController {
             $cobros_a_restar_de_precio = Cobros::where('historial_clinico_id', $h->id)->sum('cobro'); //Esto es la suma de los cobros de un item de HC
             $h->pdc = $h->precio - $cobros_a_restar_de_precio;
         }
-        return View::make ('estadisticas.morosos')->with('p_d_c' , $p_d_c);
-	}
-
-    public function morosos_cf()
-	{
-        $fecha_inicio = Input::get('fecha_inicio');
-        $fecha_inicio = explode('/', $fecha_inicio);
-        $fecha_inicio = $fecha_inicio[2]."-".$fecha_inicio[1]."-".$fecha_inicio[0];
-        $fecha_fin = Input::get('fecha_fin');
-        $fecha_fin = explode('/', $fecha_fin);
-        $fecha_fin = $fecha_fin[2]."-".$fecha_fin[1]."-".$fecha_fin[0];
-        $p_d_c = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))->where('pendiente_de_cobro', 1)
-                ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
-                ->select('historial_clinico.*', 'pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2')
-                ->get();
-        return View::make ('estadisticas.morosos')->with('p_d_c' , $p_d_c);
+        return View::make ('estadisticas.morosos')->with('p_d_c' , $p_d_c)->with('fecha_inicio', $fecha_inicio)->with('fecha_fin', $fecha_fin);
 	}
 
 	/**
@@ -172,12 +150,16 @@ class CobrosController extends \BaseController {
 	 */
 	public function show($id)
 	{
+		$fecha_fin = date('d/m/Y');
+        $fecha_primeros = date('/m/Y');
+        $fecha_inicio = "1".$fecha_primeros;
+
         $cobros = Cobros::where('cobros.paciente_id', $id)
              ->leftJoin('tipos_de_cobro', 'tipos_de_cobro.id', '=', 'cobros.tipos_de_cobro_id')
             ->leftJoin('pacientes', 'pacientes.id', '=', 'cobros.paciente_id')
             ->select('cobros.*', 'tipos_de_cobro.nombre as tc_n', 'pacientes.nombre as p_n')
             ->get();
-        return View::make('cobros.index')->with('cobros', $cobros);
+        return View::make('cobros.index')->with('cobros', $cobros)->with('fecha_inicio', $fecha_inicio)->with('fecha_fin', $fecha_fin);
 	}
 
 
