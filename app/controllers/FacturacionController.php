1cfabae1a153211c2a958d8c2a545107e9289d67
Pablo Castellano <pablo@anche.no>
2015-07-07 20:23:27 +0200

    indent

--- app/controllers/FacturacionController.php
+++ app/controllers/FacturacionController.php
@@ -20,9 +20,9 @@ class FacturacionController extends \BaseController {
 ////                var_dump($fecha_inicio);
 //                //echo "POST";
 //            } else {
-                $fecha_fin = date('d/m/Y');
-                $fecha_primeros = date('/m/Y');
-                $fecha_inicio = "1".$fecha_primeros;
+        $fecha_fin = date('d/m/Y');
+        $fecha_primeros = date('/m/Y');
+        $fecha_inicio = "1".$fecha_primeros;
 //                var_dump($fecha_fin);
 //                var_dump($fecha_inicio);
 //                echo "Sin POST";
@@ -31,9 +31,9 @@ class FacturacionController extends \BaseController {
 
 //            var_dump($fecha_actual);
 //            var_dump($primeros_de_mes);
-            $profesional = Profesional::where('user_id', Auth::user()->id )->first();
-             if(count($profesional) > 0)
-            {
+        $profesional = Profesional::where('user_id', Auth::user()->id )->first();
+        if(count($profesional) > 0)
+        {
             $historiales = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))
                     ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
                     ->select('pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', 'pacientes.id as p_id',
@@ -46,53 +46,54 @@ class FacturacionController extends \BaseController {
                     ->orderBy('fecha_realizacion', 'ASC')->get();
             //var_dump($historial);
             return View::make('facturacion.index')->with('historiales', $historiales)->with('fecha_inicio', $fecha_inicio)->with('fecha_fin', $fecha_fin);
-            } else {
-                return Redirect::action('ProfesionalController@index')->with('message', 'No existe ningún profesional asignado a su usuario. Asigne ahora uno, o dirígase a los administradores de la aplicación');
-            }
+        } else {
+            return Redirect::action('ProfesionalController@index')->with('message', 'No existe ningún profesional asignado a su usuario. Asigne ahora uno, o dirígase a los administradores de la aplicación');
+        }
     }
-        public function index_cf() //Index de facturación (u historiales) con fechas del formulario
-    {
-             $fecha_inicio = Input::get('fecha_inicio');
-                $fecha_inicio = explode('/', $fecha_inicio);
-                $fecha_inicio = $fecha_inicio[2]."-".$fecha_inicio[1]."-".$fecha_inicio[0];
-                $fecha_fin = Input::get('fecha_fin');
-                $fecha_fin = explode('/', $fecha_fin);
-                $fecha_fin = $fecha_fin[2]."-".$fecha_fin[1]."-".$fecha_fin[0];
-            //var_dump($fecha_fin);
-            //var_dump($fecha_inicio);
-            $historiales = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))
-                    ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
-                    ->select('pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', 'pacientes.id as p_id',
-                            'tratamientos.nombre as t_n',
-                            'fecha_realizacion',
-                            'abonado_quiron',
-                            'cobrado_profesional', 'historial_clinico.id as h_id', 'historial_clinico.*')
-                    ->leftJoin('tratamientos', 'historial_clinico.tratamiento_id', '=', 'tratamientos.id')
-                    ->orderBy('fecha_realizacion', 'DESC')->get();
-            //var_dump($historial);
-            return View::make('facturacion.index')->with('historiales', $historiales);
-    }
-        public function index_nc() //Index de facturación (u historiales) con fechas del formulario q están sin cobrar
-    {
-            $fecha_inicio = Input::get('fecha_inicio');
-                $fecha_inicio = explode('/', $fecha_inicio);
-                $fecha_inicio = $fecha_inicio[2]."-".$fecha_inicio[1]."-".$fecha_inicio[0];
-                $fecha_fin = Input::get('fecha_fin');
-                $fecha_fin = explode('/', $fecha_fin);
-                $fecha_fin = $fecha_fin[2]."-".$fecha_fin[1]."-".$fecha_fin[0];
 
-            $historiales = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))
-                    ->where('cobrado_profesional', 0)
-                    ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
-                    ->select('pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', 'pacientes.id as p_id',
-                            'tratamientos.nombre as t_n',
-                            'fecha_realizacion',
-                            'abonado_quiron',
-                            'cobrado_profesional', 'historial_clinico.id as h_id', 'historial_clinico.*')
-                    ->leftJoin('tratamientos', 'historial_clinico.tratamiento_id', '=', 'tratamientos.id')
-                    ->orderBy('fecha_realizacion', 'DESC')->get();
-            //var_dump($historial);
-            return View::make('facturacion.index')->with('historiales', $historiales);
+    public function index_cf() //Index de facturación (u historiales) con fechas del formulario
+    {
+        $fecha_inicio = Input::get('fecha_inicio');
+        $fecha_inicio = explode('/', $fecha_inicio);
+        $fecha_inicio = $fecha_inicio[2]."-".$fecha_inicio[1]."-".$fecha_inicio[0];
+        $fecha_fin = Input::get('fecha_fin');
+        $fecha_fin = explode('/', $fecha_fin);
+        $fecha_fin = $fecha_fin[2]."-".$fecha_fin[1]."-".$fecha_fin[0];
+
+        $historiales = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))
+                ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
+                ->select('pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', 'pacientes.id as p_id',
+                        'tratamientos.nombre as t_n',
+                        'fecha_realizacion',
+                        'abonado_quiron',
+                        'cobrado_profesional', 'historial_clinico.id as h_id', 'historial_clinico.*')
+                ->leftJoin('tratamientos', 'historial_clinico.tratamiento_id', '=', 'tratamientos.id')
+                ->orderBy('fecha_realizacion', 'DESC')->get();
+        //var_dump($historial);
+        return View::make('facturacion.index')->with('historiales', $historiales);
+    }
+
+    public function index_nc() //Index de facturación (u historiales) con fechas del formulario q están sin cobrar
+    {
+        $fecha_inicio = Input::get('fecha_inicio');
+        $fecha_inicio = explode('/', $fecha_inicio);
+        $fecha_inicio = $fecha_inicio[2]."-".$fecha_inicio[1]."-".$fecha_inicio[0];
+        $fecha_fin = Input::get('fecha_fin');
+        $fecha_fin = explode('/', $fecha_fin);
+        $fecha_fin = $fecha_fin[2]."-".$fecha_fin[1]."-".$fecha_fin[0];
+
+        $historiales = Historial_clinico::whereBetween('fecha_realizacion', array($fecha_inicio, $fecha_fin))
+                ->where('cobrado_profesional', 0)
+                ->leftJoin('pacientes', 'historial_clinico.paciente_id', '=', 'pacientes.id')
+                ->select('pacientes.nombre as p_n', 'pacientes.apellido1 as p_a1', 'pacientes.apellido2 as p_a2', 'pacientes.id as p_id',
+                        'tratamientos.nombre as t_n',
+                        'fecha_realizacion',
+                        'abonado_quiron',
+                        'cobrado_profesional', 'historial_clinico.id as h_id', 'historial_clinico.*')
+                ->leftJoin('tratamientos', 'historial_clinico.tratamiento_id', '=', 'tratamientos.id')
+                ->orderBy('fecha_realizacion', 'DESC')->get();
+        //var_dump($historial);
+        return View::make('facturacion.index')->with('historiales', $historiales);
     }
 
 
